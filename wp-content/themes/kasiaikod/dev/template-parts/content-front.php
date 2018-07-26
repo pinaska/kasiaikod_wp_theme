c88df36d0ab753462526d83cf0a5645b7f27f4db
Kate Jeziorska <k_jeziorska@yahoo.com>
2018-07-25 17:03:13 -0700

    add custom search  with categories to front page

--- wp-content/themes/kasiaikod/dev/template-parts/content-front.php
+++ wp-content/themes/kasiaikod/dev/template-parts/content-front.php
@@ -8,40 +8,44 @@
  */
 
 ?>
+<!-- content from WYSYG editor -->
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-<?php kasiaikod_post_thumbnail(); ?>
-	<header class="entry-header">
-		<div class="post-cat">
-			<?php kasiaikod_post_categories(); ?>
-		</div>
-		<?php
-
-		if ( 'post' === get_post_type() ) :
-			?>
-			<div class="entry-meta">
-				<?php
-					kasiaikod_posted_on();
-					kasiaikod_posted_by();
-					kasiaikod_comments_link();
-				?>
-			</div><!-- .entry-meta -->
-			<?php
-		endif;
-		?>
-	</header><!-- .entry-header -->
-
 	<div class="entry-content">
 		<?php the_content(); ?>
 	</div><!-- .entry-content -->
 </article><!-- #post-<?php the_ID(); ?> -->
-<?php get_search_form();?>
+
+<!-- searchbar for front page -->
+<div class="front-search-form "><?php echo ("<h2>Wyszukaj na blogu</h2> ");?>
+<?php echo('<div class="cat-list">');
+$categories = get_categories();
+foreach ( $categories as $category ) {
+	echo('<h4><a href="category/'.$category->slug.'"> '.$category->name.'</a></h4>');
+}
+echo('</div>');?>
+
+	<form role="search" method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+	<label>
+		<span class="screen-reader-text"><?php echo _x( 'Search for:', 'label', 'wprig' ); ?></span>
+		<input type="search" class="search-field" placeholder="<?php echo esc_attr_x( 'Szukaj &hellip;', 'placeholder', 'wprig' ); ?>" value="<?php echo get_search_query(); ?>" name="s" />
+	</label>
+	<button type="submit" class="search-submit"><span class="screen-reader-text"><?php echo _x( 'Search', 'submit button', 'wprig' ); ?></span></button>
+</form>
+<?php echo ("<h6><a href=#>Nie ma nic dla Ciebie? Napisz do mnie</a></h6>");?>
+</div>
+
+<!-- loop for displaying newest posts -->
 <article class="new-posts">
 		<?php $the_query = new WP_Query( 'posts_per_page=-1' ); ?>
-
+<div class="post-ctn">
 	<?php while ($the_query -> have_posts()) : $the_query -> the_post();?>
-		<a href="<?php the_permalink() ?>"><?php the_post_thumbnail('medium')?></a>
+	<div class="single-post-ctn">
+		<a href="<?php the_permalink() ?>"><?php the_post_thumbnail('large')?></a>
+		</div>
 	<?php
 	endwhile;
 	wp_reset_postdata();
 	?>
+		</div>
+
 </article>
